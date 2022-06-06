f6851add164d3dad08a7df39d6b5527b7de21b84
sajedul <98393471+sajedul43@users.noreply.github.com>
2022-06-07 02:45:59 +0900

    add jwt

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -37,9 +37,13 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
+             'api' => [
+                'driver' => 'jwt',
+                'provider' => 'users',
+            ],
     ],
 
     /*
